4484f062533f8942268f11edd78110467b9657d0
Fortunate122 <davidsaldana122@gmail.com>
2026-04-14 15:38:01 -0700

    upgrade project cards with descriptions and tech stack

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,20 @@
-function Project({ title, image, liveLink, repoLink }) {
-    return (
-      <div className="project">
-        <img src={image} alt={`${title} screenshot`} />
+function Project({ title, image, description, tech, liveLink, repoLink }) {
+  return (
+    <article className="project">
+      <img src={image} alt={`${title} screenshot`} className="project-image" />
+
+      <div className="project-content">
         <h3>{title}</h3>
+        <p className="project-description">{description}</p>
+
+        <ul className="project-tech-list">
+          {tech.map((item) => (
+            <li key={item} className="project-tech-item">
+              {item}
+            </li>
+          ))}
+        </ul>
+
         <div className="project-links">
           <a href={liveLink} target="_blank" rel="noopener noreferrer">
             Live Demo
@@ -12,8 +24,8 @@ function Project({ title, image, liveLink, repoLink }) {
           </a>
         </div>
       </div>
-    );
-  }
-  
-  export default Project;
-  
+    </article>
+  );
+}
+
+export default Project;
