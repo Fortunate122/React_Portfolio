f2d9576d2f43094caf28db79e9491b1b96072297
Fortunate122 <davidsaldana122@gmail.com>
2026-04-16 14:22:55 -0700

    resume page update

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,24 +1,23 @@
 function Resume() {
-    return (
-      <section>
-        <h2>Resume</h2>
-        <p>
-          <a href="public\Resume_DS_03-26.pdf" download>
-            Download My Resume
-          </a>
-        </p>
-        <h3>Proficiencies</h3>
-        <ul>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Node.js</li>
-          <li>HTML & CSS</li>
-          <li>Typescript</li>
-          <li>Express.js</li>
-        </ul>
-      </section>
-    );
-  }
-  
-  export default Resume;
-  
+  return (
+    <section>
+      <h2>Resume</h2>
+      <p>
+        <a href="/Resume_DS_03-26.pdf" download>
+          Download My Resume
+        </a>
+      </p>
+      <h3>Proficiencies</h3>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>HTML & CSS</li>
+        <li>Typescript</li>
+        <li>Express.js</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Resume;
